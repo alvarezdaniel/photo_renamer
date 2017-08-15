8aeda84d95cc190610f13a1bee1e735d3d8e0e87
Daniel Alvarez <alvarezd@kapsch.net>
2017-08-15 16:51:37 -0300

    Initial commit - csproj and sln

---
+++ photo_renamer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace photo_renamer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("photo-rename tool");
+
+            Console.WriteLine("Press any key to finish...");
+            Console.ReadKey();
+        }
+    }
+}
